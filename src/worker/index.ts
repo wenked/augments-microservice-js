7b366e3da6f1642deae14d13169c5ebd2076b8cd
wenked <wenked.matheus@yahoo.com.br>
2022-05-17 20:19:45 -0300

    inicializando

---
+++ src/worker/index.ts
@@ -0,0 +1,37 @@
+import client, { Connection, Channel, ConsumeMessage } from "amqplib";
+import log from "../logger";
+
+async function Worker() {
+	// consumer for the queue.
+	// We use currying to give it the channel required to acknowledge the message
+	try {
+		const consumer =
+			(channel: Channel) =>
+			(msg: ConsumeMessage | null): void => {
+				try {
+					if (msg) {
+						// Display the received message
+						log.info(msg.content.toString());
+						// Acknowledge the message
+						channel.ack(msg);
+					}
+				} catch (error) {
+					throw new Error("teste");
+				}
+			};
+		const connection: Connection = await client.connect("amqp://localhost");
+		// Create a channel
+		const channel: Channel = await connection.createChannel();
+		// Makes the queue available to the client
+		await channel.assertQueue("augments_queue", { durable: true });
+		await channel.prefetch(1);
+		// Send some messages to the queue
+		log.info(" [*] Waiting for messages in %s. To exit press CTRL+C", "augments_queue");
+		// Start the consumer
+		await channel.consume("augments_queue", consumer(channel));
+	} catch (error) {
+		log.error(`Error in worker: ${error}`);
+	}
+}
+
+Worker();
